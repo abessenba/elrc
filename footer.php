fbffd93f70a62cf05977332e5233e5b592144710
Ann Bessenbacher <ambessenbacher@purdue.edu>
2017-02-08 16:31:00 -0500

    Initial working version

---
+++ footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Footer
+ *
+ *
+ * @author 		Ann Bessenbacher
+ * @copyright	December 2016
+ */
+?>
+
+<div class="super-group-footer-wrap">
+	<div class="super-group-footer">
+		<p><strong>Evaluation and Learning Research Center</strong><br>
+		College of Education: Purdue University<br>
+		100 N. University St., West Lafayette, IN 47907<br>
+		</p>
+		&copy; 2016 Purdue University | An equal access/equal opportunity university | Copyright | Complaints<br>
+		&copy; 2016 HUBzero Foundation, LLC.<br>
+		
+		
+	</div>
+</div>
